67394ca19f5cec49b5f6df753944bbe665b0955c
tanvirtareq <cse.tanvir.17@gmail.com>
2025-08-23 18:49:21 +0600

    TJ

--- script.js
+++ script.js
@@ -12,41 +12,43 @@ function getRandomColor() {
     return color;
 }
 
-const getRandomPosition = () => {
-    const mainPanel = document.getElementById('main-panel');
-    const x = Math.random() * (mainPanel.clientWidth - 100);
-    const y = Math.random() * (mainPanel.clientHeight - 100);
+const getRandomPosition = (parent_element) => {
+    const x = Math.random() * (parent_element.clientWidth - 100);
+    const y = Math.random() * (parent_element.clientHeight - 100);
     return { x, y };
 };
 
-const getRandomCircle = () => {
+let circles = [];
+
+const getRandomCircle = (parent_element) => {
     const circle = document.createElement('div');
     circle.classList.add('circle');
     circle.style.backgroundColor = getRandomColor();
-    circle.style.height = '1px';
-    circle.style.width = '1px';
-    const pos = getRandomPosition();
-    circle.style.left = pos.x + 'px';
-    circle.style.top = pos.y + 'px';
+    let size = 1000;
+    circle.style.height = size + 'px';
+    circle.style.width = size + 'px';
+    const pos = getRandomPosition(parent_element);
+    circle.style.left = pos.x - size / 2 + 'px';
+    circle.style.top = pos.y - size / 2 + 'px';
     circle.style.position = 'absolute';
-
-    let count = 0;
-
-    // const intervalId = setInterval(() => {
-    //     count++;
-    //     const scale = 1 + count * 0.1; // grows 10% each tick
-    //     circle.style.transform = `scale(${scale})`;
-    //     if (count > 10000) {
-    //         clearInterval(intervalId);
-    //         circle.remove();
-    //     }
-    // }, 10);
-
 
     return circle;
 };
 
+const addCircle = (parent_element) => {
+    const circle = getRandomCircle(parent_element);
+    parent_element.appendChild(circle);
+    circles.push(circle);
+};
+
 setInterval(() => {
-    const circle = getRandomCircle();
-    document.getElementById('main-panel').appendChild(circle);
-}, 0.01);
+    const t_shape_element = document.getElementsByClassName('t-shape')[0];
+    addCircle(t_shape_element);
+    const j_shape_element = document.getElementsByClassName('j-shape')[0];
+    addCircle(j_shape_element);
+    if (circles.length > 10) {
+        const oldCircle = circles[Math.floor(Math.random() * circles.length)];
+        circles = circles.filter(c => c !== oldCircle);
+        t_shape_element.removeChild(oldCircle);
+    }
+}, 1000);
