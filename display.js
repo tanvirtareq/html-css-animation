019b816c4d9a354c1be28a64838d0a20e774d30a
tanvirtareq <cse.tanvir.17@gmail.com>
2025-08-25 02:22:35 +0600

    Add envelop animation

--- display.js
+++ display.js
@@ -1,21 +1,3 @@
-function fitText(panel) {
-  const maxFontSize = 200; // max font size
-  const minFontSize = 10; // min font size
-  let fontSize = maxFontSize;
-  panel.style.fontSize = fontSize + "px";
-
-  const text = panel.innerText;
-
-  while (
-    (panel.scrollWidth > panel.clientWidth ||
-      panel.scrollHeight > panel.clientHeight) &&
-    fontSize > minFontSize
-  ) {
-    fontSize -= 1;
-    panel.style.fontSize = fontSize + "px";
-  }
-}
-
 function decodeText(encoded) {
   return decodeURIComponent(escape(atob(encoded)));
 }
@@ -27,13 +9,8 @@ const code = params.get("code");
 const container = document.getElementById("text-panel");
 if (code) {
   try {
-    container.textContent = decodeText(code)
-    fitText(container);
+    container.textContent = decodeText(code);
   } catch (e) {
     container.textContent = "Invalid code!";
   }
 }
-
-window.addEventListener("load", () => {
-  document.getElementById("text-panel").classList.add("show");
-});
